64b9e843bcf41c354ef6355bf2f079a558cc823d
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2022-02-28 18:46:08 +0100

    Full application and server

--- server/api/routes/games.routes.js
+++ server/api/routes/games.routes.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const gameSchema = require("../models/games.model");
 const authorize = require("../utils/middlewares/auth.middleware");
 const characterSchema = require ("../models/character.model");
+const enemySchema = require ("../models/enemies.model")
+const locationSchema =  require ("../models/locations.model")
+const weaponSchema = require ("../models/weapon.model")
 let charactersId;
+let enemiesId;
+let weaponsId;
+let locationsId;
 
 
 // Get All Games
@@ -49,5 +55,69 @@ router.route('/games/:id/characters').get(/* authorize, */(req, res, next) => {
             return next(errorHappened.message);
         })
 })
+
+// Get All enemies of a game
+router.route('/games/:id/enemies').get(/* authorize, */(req, res, next) => {
+    const gameId = req.params.id;
+
+    gameSchema.findById(gameId).populate("enemies")
+        .then((game) => {
+            enemiesId = game.enemies;
+        })
+        
+        .then(async () => {
+            const enemies = await enemySchema.find({ _id: enemiesId.map(i => i) })
+            return res.json(enemies);
+        })
+        
+        .catch((error) => {
+            const errorHappened = new Error();
+            return next(errorHappened.message);
+        })
+})
+
+
+// Get All locations of a game
+router.route('/games/:id/locations').get(/* authorize, */(req, res, next) => {
+    const gameId = req.params.id;
+
+    gameSchema.findById(gameId).populate("locations")
+        .then((game) => {
+            locationsId = game.locations;
+        })
+        
+        .then(async () => {
+            const locations = await locationSchema.find({ _id: locationsId.map(i => i) })
+            return res.json(locations);
+        })
+        
+        .catch((error) => {
+            const errorHappened = new Error();
+            return next(errorHappened.message);
+        })
+})
+
+
+// Get All weapons of a game
+router.route('/games/:id/weapons').get(/* authorize, */(req, res, next) => {
+    const gameId = req.params.id;
+
+    gameSchema.findById(gameId).populate("weapons")
+        .then((game) => {
+            weaponsId = game.weapons;
+        })
+        
+        .then(async () => {
+            const weapons = await weaponSchema.find({ _id:weaponsId.map(i => i) })
+            return res.json(weapons);
+        })
+        
+        .catch((error) => {
+            const errorHappened = new Error();
+            return next(errorHappened.message);
+        })
+})
+
+
 
 module.exports = router;
